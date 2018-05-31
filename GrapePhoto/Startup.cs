ce3141da8608545e7c38d39a023bce331de3b704
XuYa <xuyae573@foxmail.com>
2018-05-31 23:03:47 +0800

    update system

--- GrapePhoto/Startup.cs
+++ GrapePhoto/Startup.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Amazon;
-using Amazon.Runtime.CredentialManagement;
-using Amazon.S3;
+﻿using Amazon.S3;
 using GrapePhoto.Proxy;
 using GrapePhoto.Web.Models.Account;
 using Microsoft.AspNetCore.Authentication.Cookies;
